69359a317ad632b285ec0c268ed96621f8c20efd
gunysunny <mikan31@naver.com>
2023-01-09 18:26:46 +0900

    공통  모듈 분리

--- js/main.js
+++ js/main.js
@@ -1,22 +1,6 @@
-const searchEl = document.querySelector('.search');
-const searchInputEl = searchEl.querySelector('input')
-
-searchEl.addEventListener('click', function(){
-    searchInputEl.focus();
-});
-
-searchInputEl.addEventListener('focus', function(){
-    searchEl.classList.add('focused');
-    searchInputEl.setAttribute('placeholder', '통합검색');
-});
-
-searchInputEl.addEventListener('blur', function(){
-    searchEl.classList.remove('focused');
-    searchInputEl.setAttribute('placeholder', '');
-});
-
-
 const badgeEl = document.querySelector('header .badges');
+const toTopEl = document.querySelector('#to-top');
+
 window.addEventListener('scroll' , _.throttle(function(){
     console.log('window.scrollY');
     if(window.scrollY > 500){
@@ -44,7 +28,7 @@ window.addEventListener('scroll' , _.throttle(function(){
 }, 300));
 // _.throttle(함수, 밀리세컨드시간추가(1000당 1초))
 
-const toTopEl = document.querySelector('#to-top');
+
 toTopEl.addEventListener('click', function(){
     gsap.to(window, 0.7,{
         scrollTo:0
@@ -158,5 +142,3 @@ spyEls.forEach(function(spyEl){
 });
 
 
-const thisYear = document.querySelector('.this-year');
-thisYear.textContent = new Date().getFullYear();
